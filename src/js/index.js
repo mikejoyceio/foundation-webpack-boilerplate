410a124057c86cb98ef135a3b1332f29c5713af2
Mike Joyce <contact@mikejoyce.me>
2018-08-13 03:40:13 +0700

    Add boilerplate

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * Import Foundation
+ * @external 'foundation-sites'
+ * @see {@link https://www.npmjs.com/package/foundation-sites}
+ */
+import { Foundation } from 'foundation-sites';
+
+/**
+ * Import SCSS
+ */
+import '../scss/app.scss';
